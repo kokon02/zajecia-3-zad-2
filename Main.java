4467f6af81cb1f00eadd2575be2bb63afc06304b
kokon02 <127319252+kokon02@users.noreply.github.com>
2023-04-05 12:42:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    for(int a=4; a>=0;a--){
+      System.out.println("");
+      for(int b=0; b<=a; b++){
+        System.out.print("*");
+      }
+    }
+  }
+}
